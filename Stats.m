c114bc0cb7949f5498d91caa4eddf0dca58b2775
Erik Garcia Oyono <eg424@ic.ac.uk>
2025-03-11 17:20:35 +0000

    Update Stats.m
    Added info on excel

--- Stats.m
+++ Stats.m
@@ -3,4 +3,6 @@ Total time taken: 1.4382 seconds;
 Average time per prediction: 0.0002 seconds;
 Weighted_rank = 25.3570;
 
-% In excel, weighted rank = 28.317
+% In excel:
+% RMSE = 28.109;
+% Weighted rank = 28.317;
