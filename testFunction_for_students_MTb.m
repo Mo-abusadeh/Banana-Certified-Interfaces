eff729f6367a91995397ccb40830874d33827190
Erik Garcia Oyono <eg424@ic.ac.uk>
2025-03-18 12:13:54 +0000

    SVM + Kalman (WIP)
    Classifier can be further refined by PCA.

--- testFunction_for_students_MTb.m
+++ testFunction_for_students_MTb.m
@@ -6,13 +6,13 @@
 
 function RMSE = testFunction_for_students_MTb(teamName)
 
-load monkeydata0.mat
+load monkeydata_training.mat
 
 % Set random number generator
 rng(2013);
 ix = randperm(length(trial));
 
-addpath(teamName);
+% addpath("Banana-Certified Interfaces");
 
 % Select training and testing data (you can choose to split your data in a different way if you wish)
 trainingData = trial(ix(1:50),:);
@@ -21,7 +21,8 @@ testData = trial(ix(51:end),:);
 fprintf('Testing the continuous position estimator...')
 
 meanSqError = 0;
-n_predictions = 0;  
+n_predictions = 0;
+totalTime = 0;
 
 figure
 hold on
@@ -29,9 +30,11 @@ axis square
 grid
 
 % Train Model
+tic;
 modelParameters = positionEstimatorTraining(trainingData);
 
 for tr=1:size(testData,1)
+    
     display(['Decoding block ',num2str(tr),' out of ',num2str(size(testData,1))]);
     pause(0.001)
     for direc=randperm(8) 
@@ -45,6 +48,7 @@ for tr=1:size(testData,1)
             past_current_trial.decodedHandPos = decodedHandPos;
 
             past_current_trial.startHandPos = testData(tr,direc).handPos(1:2,1); 
+            
             
             if nargout('positionEstimator') == 3
                 [decodedPosX, decodedPosY, newParameters] = positionEstimator(past_current_trial, modelParameters);
@@ -66,10 +70,16 @@ for tr=1:size(testData,1)
     end
 end
 
+elapsedTime = toc;
+totalTime = totalTime + elapsedTime;
+
 legend('Decoded Position', 'Actual Position')
 
-RMSE = sqrt(meanSqError/n_predictions) 
+RMSE = sqrt(meanSqError/n_predictions)
+fprintf('Total time taken: %.4f seconds\n', totalTime);
+fprintf('Average time per prediction: %.4f seconds\n', totalTime / n_predictions);
+Weighted_rank = 0.9 * RMSE + 0.1 * totalTime
 
-rmpath(genpath(teamName))
+rmpath(genpath('Banana-Certified Interfaces'))
 
 end
